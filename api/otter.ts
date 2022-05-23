7760f296361ec2175650c788ea9ae1fcb9be0706
Zander Martineau <hi@zander.wtf>
2022-05-23 07:22:37 +0100

    Move to use Otter, allow setting of the tag

--- api/otter.ts
+++ api/otter.ts
@@ -2,7 +2,10 @@ import type { NowRequest, NowResponse } from '@now/node'
 import axios from 'axios'
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const requestPath = `https://zmarks.zander.wtf/api/collections/zm:link??limit=${req.query.count}`
+  console.log(`🚀 ~ req.query.tag`, req.query.tag)
+  // const requestPath = `https://otter.zander.wtf/api/tag/${req.query.tag}?limit=${req.query.count}`
+  const requestPath = `http://localhost:3000/api/tag/${req.query.tag}?limit=${req.query.count}`
+  console.log(`🚀 ~ requestPath`, requestPath)
   const { data } = await axios(requestPath, {
     headers: { Authorization: `Bearer ${process.env.ZMARKS_SUPABASE_TOKEN}` },
   })
