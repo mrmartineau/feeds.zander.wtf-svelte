4b0a04df636d5060f6a4b3f95d15ce630201f58f
Zander Martineau <hi@zander.wtf>
2022-05-23 07:29:12 +0100

    Use Otter for Twitter likes

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import Bookmarks from './lib/Bookmarks.svelte'
   import GitHubStars from './lib/GitHubStars.svelte'
   import InstapaperUnread from './lib/InstapaperUnread.svelte'
-  import TwitterLikes from './lib/TwitterLikes.svelte'
   import Tailwind from './Tailwind.svelte'
 </script>
 
@@ -14,7 +13,7 @@
       <Bookmarks tag="zm:link" title="Bookmarks" />
     </div>
     <div class="w-80 flex-none">
-      <TwitterLikes />
+      <Bookmarks tag="TwitterLike" title="Twitter Likes" />
     </div>
     <div class="w-80 flex-none">
       <InstapaperUnread />
