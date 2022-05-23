d10dca721e8780d44c9b31a1e77b9716a63c7871
Zander Martineau <hi@zander.wtf>
2022-05-23 07:25:32 +0100

    Use hosted API

--- api/otter.ts
+++ api/otter.ts
@@ -2,10 +2,7 @@ import type { NowRequest, NowResponse } from '@now/node'
 import axios from 'axios'
 
 export default async (req: NowRequest, res: NowResponse) => {
-  console.log(`🚀 ~ req.query.tag`, req.query.tag)
-  // const requestPath = `https://otter.zander.wtf/api/tag/${req.query.tag}?limit=${req.query.count}`
-  const requestPath = `http://localhost:3000/api/tag/${req.query.tag}?limit=${req.query.count}`
-  console.log(`🚀 ~ requestPath`, requestPath)
+  const requestPath = `https://otter.zander.wtf/api/tag/${req.query.tag}?limit=${req.query.count}`
   const { data } = await axios(requestPath, {
     headers: { Authorization: `Bearer ${process.env.ZMARKS_SUPABASE_TOKEN}` },
   })
